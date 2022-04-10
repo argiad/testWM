110e89fb1943dee9607ae693ccbda65b41b606bc
Artem Mkrtchyan <a.s.mkrtchyan@gmail.com>
2022-04-09 20:24:44 -0400

    CI test changes

--- app/src/main/java/com/steegler/walmart/presentation/ResultState.kt
+++ app/src/main/java/com/steegler/walmart/presentation/ResultState.kt
@@ -3,7 +3,9 @@ package com.steegler.walmart.presentation
 import com.steegler.walmart.domain.model.Country
 
 data class ResultState(
+
     val isLoading: Boolean = true,
     val itemsList: List<Country> = emptyList(),
     val error: String = "",
+
 )
